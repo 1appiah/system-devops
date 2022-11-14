b0bec8f740992cff6eb0eb09d3176e960eb7bc9e
Brian-Machiestay <brianmachiestay@gmail.com>
2022-11-14 17:34:09 +0000

    export to json

---
+++ 0x15-api/2-export_to_JSON.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""gather data from an API"""
+import csv
+import json
+import requests
+from sys import argv
+
+emp_info = 'https://jsonplaceholder.typicode.com/users/'
+all_todos = 'https://jsonplaceholder.typicode.com/todos'
+if __name__ == "__main__":
+    emp_id = argv[1]
+    get_emp_info = json.loads(requests.get(emp_info + emp_id).text)
+    get_all_todos = json.loads(requests.get(all_todos).text)
+    todos_done_list = []
+    name = None
+
+    to_js = dict()
+    todos = []
+    with open('{}.csv'.format(emp_id), 'w', encoding="utf8") as f:
+        for i in get_all_todos:
+            if i['userId'] == int(emp_id):
+                this_info = {"task": i['title'], "completed": i['completed'],
+                             "username": get_emp_info['username']}
+                todos.append(this_info)
+    to_js[emp_id] = todos
+    with open("{}.json".format(emp_id), "w", encoding="utf8") as f:
+        f.write(json.dumps(to_js))
