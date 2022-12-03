87ae4eb5b0a785ba39b07ed292f7df97a21a1c24
Brian-Machiestay <brianmachiestay@gmail.com>
2022-12-03 11:45:10 +0000

    specify a path for puppet exec

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,5 +1,6 @@
 # debug the wordpress stack
 exec { 'correct file name':
-  command => 'mv /var/www/html/wp-includes/wp-settings.php\
-   /var/www/html/wp-includes/wp-settings.phpp',
+  command => 'mv /var/www/html/wp-includes/class-wp-locale.php\
+   /var/www/html/wp-includes/class-wp-locale.php',
+  path    => '/bin',
 }
